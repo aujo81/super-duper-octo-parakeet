c3a2829f40e8c3ac5a8fa2478c2197bf7d6853c3
Johan Austrin <johan.austrin@austrinconsulting.se>
2017-11-19 19:06:15 +0100

    Added getAllMessagesForYear and getAllMessagesPaginated

--- messenger/src/main/java/com/austrinconsulting/messenger/service/MessageService.java
+++ messenger/src/main/java/com/austrinconsulting/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.austrinconsulting.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -12,12 +13,32 @@ public class MessageService {
 	private Map<Long, Message> messages = DatabaseClass.getMessages();
 	
 	public MessageService() {
-		messages.put(1L, new Message(1, "Hellow World!", "Johan"));
+		messages.put(1L, new Message(1, "Hello World!", "Johan"));
 		messages.put(2L,  new Message (2, "Hello Jersey!", "Aujo"));
 	}
 	
 	public List<Message> getAllMessages() {
 		return new ArrayList<Message>(messages.values());
+	}
+	
+	public List<Message> getAllMessagesForYear(int year) {
+		List<Message> messagesForYear = new ArrayList<Message>();
+		Calendar cal = Calendar.getInstance();
+		for (Message message:messages.values()) {
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year) {
+				messagesForYear.add(message);
+			}
+		}		
+		return messagesForYear;		
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size) {
+		ArrayList<Message> list = new ArrayList<Message>(messages.values());
+		if (start + size > list.size()) {
+			return new ArrayList<Message>();
+		}
+		return list.subList(start, start+size);
 	}
 	
 	public Message getMessage(long id) {
--- messenger/src/test/java/com/austrinconsulting/messenger/MessageServiceTest.java
+++ messenger/src/test/java/com/austrinconsulting/messenger/MessageServiceTest.java
@@ -18,7 +18,7 @@ public class MessageServiceTest {
 		
 		//First message
 		assertEquals("Assert Id", testMessage1.getId(), 1L);
-		assertEquals("Assert Message Content", testMessage1.getMessage(), "Hellow World!");
+		assertEquals("Assert Message Content", testMessage1.getMessage(), "Hello World!");
 		assertEquals("Assert Message Content", testMessage1.getAuthor(), "Johan");
 				
 		//Second message
@@ -29,13 +29,13 @@ public class MessageServiceTest {
 	
 	@Test
 	public void testGetAllMessages() {
-		assertEquals("Assert size", ms.getAllMessages().size(), 2);		
+		assertEquals("Assert size", 3, ms.getAllMessages().size());		
 	}
 	
 	@Test 
 	public void testUpdateMessage() {
 		long id = 1;
-		String messageContent = "Hello World!";
+		String messageContent = "Hello World";
 		String author = "Johan A";
 		Message updatedMessage = new Message(id, messageContent, author);
 		ms.updateMessage(updatedMessage);
@@ -66,6 +66,40 @@ public class MessageServiceTest {
 		ms.removeMessage(id);
 		
 		assertEquals("Assert size", ms.getAllMessages().size(), 2);
+	}
+	
+	@Test
+	public void testGetMessagesForYear() {
+		int expectedYear = 2017;
+		assertEquals("Assert messages for year 2017",ms.getAllMessagesForYear(expectedYear).size(), 3);
+		
+		assertEquals("Assert messages for year 2016",ms.getAllMessagesForYear(expectedYear+1).size(), 0);
+		assertEquals("Assert messages for year 2016",ms.getAllMessagesForYear(expectedYear-1).size(), 0);
+		
+		assertEquals("Assert messages for year 2016",ms.getAllMessagesForYear(expectedYear+1000).size(), 0);
+		assertEquals("Assert messages for year 2016",ms.getAllMessagesForYear(expectedYear-1000).size(), 0);
+	}
+	
+	@Test
+	public void testGetAllMassagesPaginated() {
+		
+		//Setup (add 10 new messages)
+		for(int i=2;i<12;i++) {
+			ms.addMessage(new Message(i, "Message #"+i, "Me"));
+		}
+		assertEquals("Assert successful Setup()", 12, ms.getAllMessages().size());
+		
+		//Actual tests...
+		assertEquals("Assert 5 page response", 5, ms.getAllMessagesPaginated(0, 5).size());
+		assertEquals("Assert 10 page response", 10, ms.getAllMessagesPaginated(1, 10).size());
+		assertEquals("Assert 12 (all) page response", 12, ms.getAllMessagesPaginated(0, 12).size());		
+		assertEquals("Assert empty response", 0, ms.getAllMessagesPaginated(1, 12).size());		
+		
+		//Tear down (remove the created messages
+		for(int a=2;a<12;a++) {			
+			ms.removeMessage(a);			
+		}
+		assertEquals("Assert successful TearDown()", 2, ms.getAllMessages().size());		
 	}
 	
 
